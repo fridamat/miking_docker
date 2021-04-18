8e093570d748b8e3de07cece06385ab9f08f4faa
Adrien Martinez <adrien.martinez@flir.com>
2021-04-18 20:12:55 +0200

    Image version on docker build

--- run.sh
+++ run.sh
@@ -10,16 +10,16 @@ action=$1
 if [ "$action" = "build" ]
 then
     cd "$DOCKER_DIR"
-    docker build \
-	   -t "$DOCKER_BUILD_IMAGE" \
+    docker build                                     \
+	   -t ${DOCKER_BUILD_IMAGE}:${IMAGE_VERSION} \
 	   .
 else
-    docker run --tty --interactive --rm                                                                   \
-	   --network="host"                                                                               \
-	   --volume ${SSH_AUTH_SOCK}:${CONTAINER_HOME}/ssh-agent.socket                                   \
-	   --env SSH_AUTH_SOCK=${CONTAINER_HOME}/ssh-agent.socket                                         \
-	   --volume ${HOME}/.ssh/known_hosts:${CONTAINER_HOME}/.ssh/known_hosts:ro                        \
-	   --volume ${PWD}:${CONTAINER_HOME}/src:rw                                                       \
-	   ${DOCKER_BUILD_IMAGE}                                                                          \
+    docker run --tty --interactive --rm                                             \
+	   --network="host"                                                         \
+	   --volume ${SSH_AUTH_SOCK}:${CONTAINER_HOME}/ssh-agent.socket             \
+	   --env SSH_AUTH_SOCK=${CONTAINER_HOME}/ssh-agent.socket                   \
+	   --volume ${HOME}/.ssh/known_hosts:${CONTAINER_HOME}/.ssh/known_hosts:ro  \
+	   --volume ${PWD}:${CONTAINER_HOME}/src:rw                                 \
+	   ${DOCKER_BUILD_IMAGE}:${IMAGE_VERSION}                                   \
 	   /bin/bash
 fi
